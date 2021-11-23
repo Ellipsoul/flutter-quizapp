4c5f9be67ed2b8abf5180d52479b5ade46ed878f
Aron Teh <aronteh.chess@gmail.com>
2021-11-23 17:58:42 +0000

    Mapped fetched Firestore topics to a GridView

--- lib/shared/error.dart
+++ lib/shared/error.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+// Generic centered text message
+class ErrorMessage extends StatelessWidget {
+  final String message;
+
+  const ErrorMessage({Key? key, this.message = 'Error'}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(message),
+    );
+  }
+}
--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/shared/bottom_nav.dart';
+import 'package:quizapp/services/firestore.dart';
+import 'package:quizapp/shared/shared.dart';
+
+import 'package:quizapp/services/models.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavBar(),
+    return FutureBuilder<List<Topic>>(
+      future: FirestoreService().getTopics(), // Retrieve topics from firestore
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingScreen();
+        } else if (snapshot.hasError) {
+          return Center(
+            child: ErrorMessage(message: snapshot.error.toString()),
+          );
+        } else if (snapshot.hasData) {
+          // Main logic contained in here
+          var topics = snapshot.data!; // Typed in FutureBuilder
+
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.deepPurple,
+              title: const Text('Topics'),
+            ),
+            // Gridview,count puts a fixed number of items on the x axis
+            body: GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20.0),
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: topics.map((topic) => Text(topic.title)).toList(),
+            ),
+            bottomNavigationBar: const BottomNavBar(),
+          );
+        } else {
+          return const Text('No topics found in Firestore. Check database');
+        }
+      },
     );
   }
 }
